c0bc61d15c500ff7ee24dcd6ebfc91b1907ae844
Harsh K <hersh.k17@gmail.com>
2023-12-15 01:06:12 -0500

    fixed color transition bug

--- components/sections/Experience.tsx
+++ components/sections/Experience.tsx
@@ -12,29 +12,31 @@ export const Experience = () => {
 
       <ol className="relative border-s border-violet-100 dark:border-violet-900 space-y-14">
         {EXPERIENCES.map((experience, idx) => (
-          <li key={idx} className="ms-4 ml-8">
+          <li key={idx} className="ms-4">
             <div
               className={cn(
                 "absolute rounded-full w-3 h-3 mt-1.5 -start-1.5",
                 idx === 0 ? "bg-violet-500" : "bg-violet-200 dark:bg-violet-900"
               )}
             ></div>
-            <Image
-              src={experience.img}
-              alt={experience.company}
-              height={40}
-              className="max-w-[200px] mt-3 mb-5"
-            />
-            <h2 className="text-xl font-semibold">{experience.title}</h2>
-            <h3>{experience.location}</h3>
-            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-              {experience.date}
-            </time>
-            <ul className="mt-3 list-disc ml-4">
-              {experience.responsibilities.map((responsibility, idx) => (
-                <li key={idx}>{responsibility}</li>
-              ))}
-            </ul>
+            <article className="ml-5">
+              <Image
+                src={experience.img}
+                alt={experience.company}
+                height={40}
+                className="max-w-[200px] mt-3 mb-5"
+              />
+              <h2 className="text-xl font-semibold">{experience.title}</h2>
+              <h3>{experience.location}</h3>
+              <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                {experience.date}
+              </time>
+              <ul className="mt-3 list-disc ml-4">
+                {experience.responsibilities.map((responsibility, idx) => (
+                  <li key={idx}>{responsibility}</li>
+                ))}
+              </ul>
+            </article>
           </li>
         ))}
       </ol>
